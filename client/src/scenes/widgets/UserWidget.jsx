013ac6daf572ee0f5ddae641b0c08b153d14dbda
abhyudaya001 <60521322+abhyudaya001@users.noreply.github.com>
2023-02-25 14:54:24 +0530

    profile-container completed

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import SampleUser from "../assets/sampleuser2.svg";
+import ProfileSetting from "../assets/profilesetting.svg";
+import GradYear from "../assets/gradyear.svg";
+import College from "../assets/college.svg";
+import Github from "../assets/github.svg";
+import LinkedIn from "../assets/linkedin.svg";
+import Hr from "../assets/line.svg";
+import "./Post.css";
 const UserWidget = ({ userId }) => {
   const [user, setUser] = useState(null);
   const token = useSelector((state) => state.token);
@@ -21,28 +29,30 @@ const UserWidget = ({ userId }) => {
   return (
     <div className="user-container">
       <div className="general-info">
-        <img src="" alt="dp" />
+        <img src={SampleUser} alt="dp" />
         <div className="general-info-detail">
           <div className="general-info-name">
             {firstName} {lastName}
           </div>
           <div className="general-info-friends">{friends.length}</div>
         </div>
-        <img src="" alt="account-setting" />
+        <img src={ProfileSetting} alt="account-setting" />
       </div>
+      <img src={Hr} alt="" />
       <div className="college-info">
         <div className="college-info-box">
-          <img src="" alt="university" />
+          <img src={College} alt="university" />
           <div className="college-info-collegename">{college}</div>
         </div>
         <div className="college-info-box">
-          <img src="" alt="passing-year" />
+          <img src={GradYear} alt="passing-year" />
           <div className="graduation year">{graduation_year}</div>
         </div>
       </div>
+      <img src={Hr} alt="" />
       <div className="other-socials">
-        <img src="" alt="github" className="github" />
-        <img src="" alt="linkedin" className="linkedin" />
+        <img src={Github} alt="github" className="github" />
+        <img src={LinkedIn} alt="linkedin" className="linkedin" />
       </div>
     </div>
   );
